ac7cc64b3aecd6c2b6fe0d6859ee24b9d0944f70
Linus Elander <linus@elander.nu>
2017-10-10 21:44:33 +0200

    Working git branch info in bash prompt

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=( "vimrc" "tmux.conf" "eslintrc.json" "dircolors")
+files=( "vimrc" "tmux.conf" "eslintrc.json" "dircolors" "bashrc")
 backup_folder=$(pwd)'/backup'
 
 # If script is executed without parameters we only print help info
