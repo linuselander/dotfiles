1b272a323152a8a36598f17ed36183d290c4d41a
Linus Elander <linus@elander.nu>
2017-10-23 21:07:47 +0200

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=( "vimrc" "tmux.conf" "eslintrc.json" "dircolors" "bashrc")
+files=( "vimrc" "tmux.conf" "eslintrc.json" "dircolors" "bashrc" "gitconfig" )
 backup_folder=$(pwd)'/backup'
 
 # If script is executed without parameters we only print help info
